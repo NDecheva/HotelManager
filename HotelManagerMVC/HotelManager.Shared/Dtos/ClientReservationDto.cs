31345e66c87324bc79d102b03275a62f62b4ddbb
unknown <siyanadelcheva2006@abv.bg>
2024-11-26 21:13:36 +0200

    HM-4 Created DTOs

---
+++ HotelManagerMVC/HotelManager.Shared/Dtos/ClientReservationDto.cs
@@ -0,0 +1,16 @@
+﻿using HotelManager.Shared.Enum;
+using System.Collections.Generic;
+
+namespace HotelManager.Shared.Dtos
+{
+    public class ClientReservationDto : BaseModel
+    {
+        public int ClientId { get; set; }
+
+        public ClientDto Client { get; set; }
+
+        public int ReservationId { get; set; }
+
+        public ReservationDto Reservation { get; set; }
+    }
+}
---
+++ HotelManagerMVC/HotelManager.Shared/Dtos/RoomDto.cs
@@ -0,0 +1,32 @@
+﻿using HotelManager.Shared.Enum;
+using System.Collections.Generic;
+
+namespace HotelManager.Shared.Dtos
+{
+    public class RoomDto : BaseModel
+    {
+        public RoomDto()
+        {
+            this.Reservations = new List<ReservationDto>();
+        }
+
+        public int RoomNumber { get; set; }
+
+        public int Capacity { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public decimal PricePerNightAdult { get; set; }
+
+        public decimal PricePerNightChild { get; set; }
+
+        public RoomType RoomType { get; set; }
+
+        public ICollection<ReservationDto> Reservations { get; set; }
+
+        public decimal BreakfastPrice { get; set; }
+
+        public decimal AllInclusivePrice { get; set; }
+
+    }
+}
