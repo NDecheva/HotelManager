a2d6709f396cec9e5b4e1100f263951e05fb9f16
ivomihailov06 <mihailovivo1@gmail.com>
2024-12-02 10:06:51 +0200

    Fixed the formating of BaseRepo and checked the code coverage

--- HotelManagerMVC/HotelManager.Data/Repos/BaseRepository .cs
+++ HotelManagerMVC/HotelManager.Data/Repos/BaseRepository .cs
@@ -10,177 +10,179 @@ using System.Linq;
 using System.Threading.Tasks;
 using HotelManager.Shared.Repos.Contracts;
 
-public abstract class BaseRepository<T, TModel>
-    : IBaseRepository<TModel>, IDisposable
+public abstract class BaseRepository<T, TModel> : IBaseRepository<TModel>, IDisposable
     where T : class, IBaseEntity
-	where TModel : BaseModel
+    where TModel : BaseModel
 {
-	protected readonly DbContext _context;
-	protected readonly DbSet<T> _dbSet;
-	protected readonly IMapper mapper;
-	private bool disposedValue;
+    protected readonly DbContext _context;
+    protected readonly DbSet<T> _dbSet;
+    protected readonly IMapper _mapper;
+    private bool _disposedValue;
 
-	protected BaseRepository(HotelManagerDbContext context,IMapper mapper)
+    protected BaseRepository(HotelManagerDbContext context, IMapper mapper)
     {
-		_context = context;
-		_dbSet = _context.Set<T>();
-		this.mapper = mapper;
-	}
-
-	public virtual TModel MapToModel(T entity)
-    {
-		return mapper.Map<TModel>(entity);
-	}
-	public virtual T MapToEntity(TModel model)
-	{
-		return mapper.Map<T>(model);
-	}
-	public virtual IEnumerable<TModel> MapToEnumerableOfModel(IEnumerable<T> entities)
-	{
-		return mapper.Map<IEnumerable<TModel>>(entities);
-	}
-
-	public async Task<IEnumerable<TModel>> GetAllAsync()
-    {
-		return this.MapToEnumerableOfModel(await _dbSet.ToListAsync());
+        _context = context;
+        _dbSet = _context.Set<T>();
+        _mapper = mapper;
     }
 
-	public async Task<TModel> GetByIdAsync(int id)
+    public virtual TModel MapToModel(T entity)
     {
-		var user = await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
-        if (user == null)
+        return _mapper.Map<TModel>(entity);
+    }
+
+    public virtual T MapToEntity(TModel model)
+    {
+        return _mapper.Map<T>(model);
+    }
+
+    public virtual IEnumerable<TModel> MapToEnumerableOfModel(IEnumerable<T> entities)
+    {
+        return _mapper.Map<IEnumerable<TModel>>(entities);
+    }
+
+    public async Task<IEnumerable<TModel>> GetAllAsync()
+    {
+        var entities = await _dbSet.ToListAsync();
+        return MapToEnumerableOfModel(entities);
+    }
+
+    public async Task<TModel> GetByIdAsync(int id)
+    {
+        var entity = await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
+        if (entity == null)
         {
-			throw new ArgumentNullException(nameof(user));
+            throw new ArgumentNullException(nameof(entity));
         }
-		return this.MapToModel(user);
+        return MapToModel(entity);
     }
 
-	public async Task CreateAsync(TModel model)
+    public async Task CreateAsync(TModel model)
     {
         if (model == null)
         {
-			throw new ArgumentNullException(nameof(model));
+            throw new ArgumentNullException(nameof(model));
         }
 
         try
         {
-			var entity = this.MapToEntity(model);
-			await _dbSet.AddAsync(entity);
-			await _context.SaveChangesAsync();
+            var entity = MapToEntity(model);
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
         catch (SqlException ex)
         {
-			//Here we can save these errors in some logs or telemetery
-			await Console.Out.WriteLineAsync($"The system threw an sql exception trying to create {nameof(model)}: {ex.Message}");
+            // Log the SQL exception
+            await Console.Out.WriteLineAsync($"SQL Exception while creating {nameof(model)}: {ex.Message}");
         }
-		catch (Exception ex)
-		{
-			//Here we can save these errors in some logs or telemetery
-			await Console.Out.WriteLineAsync($"The system threw a non-sql exception trying to create {nameof(model)}: {ex.Message}");
-		}
-	}
+        catch (Exception ex)
+        {
+            // Log the non-SQL exception
+            await Console.Out.WriteLineAsync($"Exception while creating {nameof(model)}: {ex.Message}");
+        }
+    }
 
-	public async Task UpdateAsync(TModel model)
-	{
-		if (model == null)
-		{
-			throw new ArgumentNullException(nameof(model));
-		}
-
-		try
-		{
-			var entity = await this._dbSet.FindAsync(model.Id);
-            if (entity == null)
-            {
-				throw new ArgumentNullException(nameof(entity));
-			}
-			_context.Entry(entity).CurrentValues.SetValues(model);
-			await _context.SaveChangesAsync();
-		}
-		catch (SqlException ex)
-		{
-			//Here we can save these errors in some logs or telemetery
-			await Console.Out.WriteLineAsync($"The system threw an sql exception trying to create {nameof(model)}: {ex.Message}");
-		}
-		catch (Exception ex)
-		{
-			//Here we can save these errors in some logs or telemetery
-			await Console.Out.WriteLineAsync($"The system threw a non-sql exception trying to create {nameof(model)}: {ex.Message}");
-		}
-	}
-
-
-	public async Task SaveAsync(TModel model)
+    public async Task UpdateAsync(TModel model)
     {
         if (model == null)
         {
-			throw new ArgumentNullException(nameof(model));
-		}
-		if (model.Id != 0)
+            throw new ArgumentNullException(nameof(model));
+        }
+
+        try
+        {
+            var entity = await _dbSet.FindAsync(model.Id);
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            _context.Entry(entity).CurrentValues.SetValues(model);
+            await _context.SaveChangesAsync();
+        }
+        catch (SqlException ex)
+        {
+            // Log the SQL exception
+            await Console.Out.WriteLineAsync($"SQL Exception while updating {nameof(model)}: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            // Log the non-SQL exception
+            await Console.Out.WriteLineAsync($"Exception while updating {nameof(model)}: {ex.Message}");
+        }
+    }
+
+    public async Task SaveAsync(TModel model)
+    {
+        if (model == null)
+        {
+            throw new ArgumentNullException(nameof(model));
+        }
+
+        if (model.Id != 0)
         {
             await UpdateAsync(model);
         }
-		else
-			await CreateAsync(model);
-    }
-
-	public async Task DeleteAsync(int id)
-	{
-		var entity = await this._dbSet.FindAsync(id);
-
-		if (entity == null)
-		{
-			throw new ArgumentNullException(nameof(entity));
-		}
-
-		try
-		{
-			_dbSet.Remove(entity);
-			await _context.SaveChangesAsync();
-		}
-		catch (SqlException ex)
-		{
-			//Here we can save these errors in some logs or telemetery
-			await Console.Out.WriteLineAsync($"The system threw an sql exception trying to create {nameof(entity)}: {ex.Message}");
-		}
-		catch (Exception ex)
-		{
-			//Here we can save these errors in some logs or telemetery
-			await Console.Out.WriteLineAsync($"The system threw a non-sql exception trying to create {nameof(entity)}: {ex.Message}");
-		}
-	}
-
-	public Task<bool> ExistsByIdAsync(int id)
-    {
-		return _dbSet.AnyAsync(e => e.Id == id);
-    }
-
-	public async Task<IEnumerable<TModel>> GetWithPaginationAsync(int pageSize,int pageNumber)
-    {
-		var paginatedRecords = await _dbSet
-			.Skip((pageNumber - 1) * pageSize)
-			.Take(pageSize)
-			.ToListAsync();
-
-		return MapToEnumerableOfModel(paginatedRecords);
-    }
-
-	protected virtual void Dispose(bool disposing)
-    {
-        if (!disposedValue)
+        else
         {
-            if (disposing)
-            {
-				_context.Dispose();
-            }
-			disposedValue = true;
+            await CreateAsync(model);
         }
     }
 
-
-	public void Dispose()
+    public async Task DeleteAsync(int id)
     {
-		Dispose(disposing: true);
-		GC.SuppressFinalize(this);
+        var entity = await _dbSet.FindAsync(id);
+        if (entity == null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        try
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+        catch (SqlException ex)
+        {
+            // Log the SQL exception
+            await Console.Out.WriteLineAsync($"SQL Exception while deleting {nameof(entity)}: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            // Log the non-SQL exception
+            await Console.Out.WriteLineAsync($"Exception while deleting {nameof(entity)}: {ex.Message}");
+        }
+    }
+
+    public Task<bool> ExistsByIdAsync(int id)
+    {
+        return _dbSet.AnyAsync(e => e.Id == id);
+    }
+
+    public async Task<IEnumerable<TModel>> GetWithPaginationAsync(int pageSize, int pageNumber)
+    {
+        var paginatedRecords = await _dbSet
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return MapToEnumerableOfModel(paginatedRecords);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            _disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
